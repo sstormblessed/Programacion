eee26442cc8f3e94a3948e9c0587461d41658cd8
Nicolás Iván Vázquez Pintos <90836758+sstormblessed@users.noreply.github.com>
2022-02-14 09:23:54 +0100

    Add files via upload

---
+++ Programacion/1º Evaluacion/repaso/ejercicio11.java
@@ -0,0 +1,14 @@
+package repaso;
+
+import java.util.Scanner;
+
+public class ejercicio11 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a=0;
+		
+		
+		
+	}
+
+}
---
+++ Programacion/1º Evaluacion/repaso/ejercicio13.java
@@ -0,0 +1,10 @@
+package repaso;
+
+import java.util.Scanner;
+
+public class ejercicio13 {
+
+	public static void main(String[] args) {
+		
+	}
+}
